cf3fb8bc25746c86d4d2202e72362571f7b582ac
bkmzli1 <43925220+bkmzli1@users.noreply.github.com>
2019-04-04 19:11:29 +0500

    project_euler
    Signed-off-by: bkmzli1 <43925220+bkmzli1@users.noreply.github.com>

--- delitel/src/ru/bkmz/euler/numer_12/Main.java
+++ delitel/src/ru/bkmz/euler/numer_12/Main.java
@@ -2,6 +2,22 @@ package ru.bkmz.euler.numer_12;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        int input = 0, p = 0, result = 0;
+        while (input < 500) {
+            input = 0;
+            p += 1;
+            result += p;
+            if (result % 2 == 0) {
+                System.out.println("p:" + p);
+                System.out.println("result:" + result);
+                for (int i = 1; i <= Math.sqrt(result); i++) {
+                    if (result % i == 0) {
+                        input += 2;
+                    }
+                }
+            }
+        }
+        System.out.println(result);
+
     }
 }
