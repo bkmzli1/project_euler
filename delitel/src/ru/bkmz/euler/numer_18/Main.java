d2882c7888aba430c78151a226cf6434b49634ba
bkmzli1 <43925220+bkmzli1@users.noreply.github.com>
2019-04-05 09:32:17 +0500

    project_euler
    Signed-off-by: bkmzli1 <43925220+bkmzli1@users.noreply.github.com>

---
+++ delitel/src/ru/bkmz/euler/numer_18/Main.java
@@ -0,0 +1,44 @@
+package ru.bkmz.euler.numer_18;
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        //triangle.txt with large data set works as well
+        Scanner scanner = new Scanner(new File("simple_triangle.txt"));
+
+        ArrayList<int[]> triangle = new ArrayList<>();
+
+        int counter = 1;
+        int answer;
+
+        while (scanner.hasNext()) {
+            int[] arr = new int[counter];
+
+            for (int i = 0; i < counter; i++) {
+                arr[i] = scanner.nextInt();
+            }
+
+            triangle.add(counter - 1, arr);
+            counter++;
+
+        }
+
+        for (int j = triangle.size() - 1; j > 0; j--) {
+            for (int k = 0; k <= j - 1; k++) {
+                triangle.get(j - 1)[k] = triangle.get(j - 1)[k] + Math.max(triangle.get(j)[k], triangle.get(j)[k + 1]);
+            }
+        }
+
+
+        answer = triangle.get(0)[0];
+        System.out.println(answer);
+
+
+    }
+}
