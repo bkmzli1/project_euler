1f363dcc0c28f81079bb9543f35ab4204b1ca4d3
bkmzli1 <43925220+bkmzli1@users.noreply.github.com>
2019-04-06 17:06:07 +0500

    project_euler
    Signed-off-by: bkmzli1 <43925220+bkmzli1@users.noreply.github.com>

--- delitel/src/ru/bkmz/euler/numer_22/Main.java
+++ delitel/src/ru/bkmz/euler/numer_22/Main.java
@@ -8,37 +8,45 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "res\\names.txt", namesDell;
-        Optional<String> line = null;
-        ArrayList<String> name = new ArrayList<String>();
-        String[] names;
+        String fileName = "res\\names.txt", namesDell;//адрес текстого файла
+        Optional<String> line = null;//Здесь будет храниться информацыя из файла
+        ArrayList<String> name = new ArrayList<String>();//Здесь будет храниться все имена
+        String[] names;// здесь будут все имена
         long lName, lSumName = 0, numerName = 0;
-        String alfavit = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        char[] alfavitC = alfavit.toCharArray();
-        Map alfavitMap = new HashMap();
+        String alfavit = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";//хранилище англ букв
+        Map alfavitMap = new HashMap();//коллекцыя которая будет довать,что A = 1 B = 2 C =3 и тд
+        char[] alfavitC = alfavit.toCharArray();//разбитие alfavit на отдельные буквы
+
         try {
-            line = Files.lines(Paths.get(fileName)).findFirst();
+            line = Files.lines(Paths.get(fileName)).findFirst();//загрузка файла
         } catch (IOException e) {
             e.printStackTrace();
         }
+        /*
+         * поскольку line имеет вид Option то при его перемещении в String мы получим Optional[....]
+         * по этому мы очещаем его от ненужных нам компонентов при этом остовляя запятые
+         */
         namesDell = line.toString();
         namesDell = namesDell.replace("Optional[", "");
         namesDell = namesDell.replace(String.valueOf('"'), "");
         namesDell = namesDell.replace("]", "");
 
 
-        names = namesDell.split(",");
-        name.addAll(Arrays.asList(names));
-        Collections.sort(name);
+        names = namesDell.split(",");//разделяем names на отдельные части при помощи запятых
+        name.addAll(Arrays.asList(names));//загружаем names в колекцыю
+        Collections.sort(name);//сортируем колекцыю
+        /*
+         *присваевам каждой букве англ алфавита цыфру
+         */
         for (int i = 0; i < alfavitC.length; i++) {
             alfavitMap.put(alfavitC[i], i);
         }
-        for (Object o :
-                name) {
+        for (Object o : name) {// при помощи for мы перебераем всю коллекцыю с именами
+            //полчаем имя и разделяем его на буквы
             String s = String.valueOf(o);
             char[] chars = s.toCharArray();
-            lName = 0;
-            int k = 0;
+            lName = 0;//это сумма всех символов
+            int k = 0;//для определения ставить знак + или нет
             for (int i = 0; i < chars.length; i++) {
 
                 lName += (int) alfavitMap.get(chars[i]) + 1;
@@ -52,8 +60,8 @@ public class Main {
 
             }
             System.out.println("=" + lName);
-            numerName++;
-            lSumName += lName * numerName;
+            numerName++;//номер имени
+            lSumName += lName * numerName;//умножаем номер имени на сумму имени
 
         }
         System.out.println(lSumName);
